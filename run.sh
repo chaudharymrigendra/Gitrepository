c7f63aa5560c1312660da765748d8e590fe505f5
chaudhary <chaudhary.mrigendra@gmail.com>
2023-07-07 17:44:05 +0545

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Initializing the spark Job ....."
+
+echo $(pwd)
+
+echo $(whoami)
+# Run spark job
+/home/hadoop/spark-3.3.2-bin-hadoop3/bin/spark-submit --conf spark.yarn.am.memory=1g --conf spark.executor.cores=2 --driver-memory 1g --executor-memory 2g /home/hadoop/pythonScript/TestScript.py
+
+status=$?
+if [ $status -eq 0 ]; then
+    echo "************** Saprk Job Completed Successfully *************"
+else
+    echo "!! Spark job failed while execution, plase check error log for more details !!"
+fi
+
+sleep 5
