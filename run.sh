b9c1420a70c3d729f027f00a62a4705fc84b3532
chaudhary <chaudhary.mrigendra@gmail.com>
2023-07-08 13:55:59 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo $(pwd)
 
 echo $(whoami)
 # Run spark job
-/home/hadoop/spark-3.3.2-bin-hadoop3/bin/spark-submit --conf spark.yarn.am.memory=1g --conf spark.executor.cores=2 --driver-memory 1g --executor-memory 2g /home/hadoop/pythonScript/TestScript.py
+/home/hadoop/spark-3.3.2-bin-hadoop3/bin/spark-submit --conf spark.yarn.am.memory=1g --conf spark.executor.cores=2 --driver-memory 1g --executor-memory 2g /home/hadoop/mig/TestScript.py
 
 status=$?
 if [ $status -eq 0 ]; then
